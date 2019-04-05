f4a830e1246d25c8f63e989acff9fc47b145cbdb
Tom Winget <towinget@cisco.com>
2019-04-05 13:44:58 -0700

    Initial commit

---
+++ dadbot.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.token;
+
+client.on('ready', () => {console.log('Dad bot reporting for duty');});
+
+var dadPattern = /^(?:i'm|im|i am) ([^\.\,\n]*)/i
+
+client.on('message', message => {
+    if (message.author.bot) return;
+
+    var messageContent = message.content;
+    var react = messageContent.match(dadPattern);
+
+    console.log("Got message: "+messageContent);
+    console.log("Got reaction: "+react);
+
+    if (react) {
+        message.channel.send("Hi "+react[1]+", I'm Dadbot!");
+        console.log("Said hi to: "+react[1]);
+    }
+});
+
+client.login(token);
