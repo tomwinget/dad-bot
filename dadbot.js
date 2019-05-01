6a63b15d455c8d0cf8c91a73639e94fafac17940
Tom Winget <towinget@cisco.com>
2019-05-01 13:56:26 -0700

    Adding dad jokes

--- dadbot.js
+++ dadbot.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const exec = require('child_process').execSync;
 const client = new Discord.Client();
 const token = process.env.token;
 
@@ -8,6 +9,12 @@ var dadPattern = /^(?:i’m|i'm|im|i am) ?a? ([^\.\,\n]*)/i
 
 client.on('message', message => {
     if (message.author.bot) return;
+
+    if(message.isMentioned(client.user)){
+        var output = exec('curl -H \'Accept: text/plain\' -H \'User-Agent: dad-bot (github.com/tomwinget/dad-bot)\' https://icanhazdadjoke.com/', {encoding: 'utf-8'});
+        console.log('Got joke: '+output);
+        message.channel.send(output);
+    }
 
     var messageContent = message.content;
     var react = messageContent.match(dadPattern);
