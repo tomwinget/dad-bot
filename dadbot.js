24c0bd42f0c40fee51b313b361bb715eb5c11953
Tom <tomwinget@gmail.com>
2019-10-08 10:46:02 -0700

    Nicknaming and loudness

--- dadbot.js
+++ dadbot.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
 const exec = require('child_process').execSync;
 const client = new Discord.Client();
-const token = process.env.token;
+const token = process.env.dadToken;
 
 client.on('ready', () => {console.log('Dad bot reporting for duty');});
 
-var dadPattern = /^(?:i’m|i'm|im|i am) ?a? ([^\.\,\n]*)/i
+var dadPattern = /^(?:i’m|i'm|im|i am) ?a? ([^\.\,\n]*)/i;
+var alphaPattern = /^[^a-z]+$/i;
 
 client.on('message', message => {
     if (message.author.bot) return;
@@ -19,12 +20,23 @@ client.on('message', message => {
     var messageContent = message.content;
     var react = messageContent.match(dadPattern);
 
-    console.log("Got message: "+messageContent);
-    console.log("Got reaction: "+react);
-
     if (react) {
         message.channel.send("Hi "+react[1]+", I'm Dadbot!");
         console.log("Said hi to: "+react[1]);
+        message.member.setNickname(react[1].substr(0,32))
+            .then(console.log("Set nickname for "+message.member.displayName))
+            .catch(console.error);
+    }
+
+    var alpha = messageContent.match(alphaPattern);
+    if (!alpha && messageContent.length > 0 && messageContent === messageContent.toUpperCase()
+        && messageContent != "LOL" && messageContent != "LMAO"){
+        let user = message.author;
+        var loudMessages = [`Now calm down ${user}!`, `No yelling in the house
+            ${user}!`, `Lower your voice ${user}!`, `You're being too loud ${user}!`];
+        var msg = loudMessages[Math.floor(Math.random() * loudMessages.length)];
+        message.channel.send(msg);
+        console.log("Told "+user+" to calm down");
     }
 });
 
