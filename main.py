3ac7209d96817fbd7a9c10cf1b7004f4be60ddfd
Tom <tomwinget@gmail.com>
2020-02-13 14:21:03 -0800

    Fix regex infinite loop, add uptime

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 import os
 import re
 import time
+from datetime import datetime
 import random
 import requests
 import logging
@@ -11,17 +12,18 @@ import discord
 
 TOKEN = os.environ['dadToken']
 vape_id = 436581339119222785
-client = discord.Client(max_message=None, heartbeat_timeout=120, assume_unsync_clock=True)
+client = discord.Client(max_message=None, heartbeat_timeout=30, assume_unsync_clock=True)
 
 
-dadPattern = r"^(?:i’m|i'm|im|i am) ?a? ([^\.\,\n]*)"
-alphaPattern = r"^(\D+?\s+\D+?)+$"
+dadPattern = r"^(?:i’m|i'm|im|i am) ?a? ([\w ]*)"
+alphaPattern = r"^(\w+?\s+\w+?)+$"
 loudMessages = [
     'Now calm down {}!',
     'No yelling in the house {}!',
     'Lower your voice {}!',
     'You\'re being too loud {}!'
 ]
+common_encoding = 'utf-8'
 
 
 logger = logging.getLogger('discord')
@@ -43,7 +45,7 @@ def get_joke():
         },timeout=5
     )
     if response:
-        response.encoding = 'utf-8'
+        response.encoding = common_encoding
         return response.text
     else:
         return "Sorry I couldn't think of a joke right now :/"
@@ -56,21 +58,29 @@ async def on_message(message):
         return
 
     if client.user in message.mentions:
-        logger.info("joking with %s", str(message.author))
-        await message.channel.send(get_joke())
+        if "uptime" in str(message.content):
+            timedelt = datetime.now() - start_time
+            logger.info("Sharing uptime of %s", str(timedelt))
+            await message.channel.send("I've been up for "+str(timedelt))
+        else:
+            logger.info("joking with %s", str(message.author))
+            await message.channel.send(get_joke())
     
     if "boomer" in str(message.content).lower():
         logger.info("boommering on %s", str(message.author))
         await message.channel.send("Hey that's our word!!")
-    
-    groups = re.match(dadPattern, message.content, re.IGNORECASE)
+
+
+    logger.info("Checking for dadding")
+    groups = re.search(dadPattern, str(message.content), re.IGNORECASE)
     if groups:
         logger.info("dading on %s", str(message.author))
         await message.channel.send(f'Hi {groups.group(1)}, I\'m Dadbot!')
         await message.author.edit(nick=groups.group(1)[:32])
 
-    if re.match(alphaPattern, message.content, re.IGNORECASE) \
-       and message.content == message.content.upper():
+    logger.info("Checking for yelling")
+    if re.search(alphaPattern, str(message.content), re.IGNORECASE) \
+       and str(message.content) == str(message.content).upper():
         logger.info("silencing %s", str(message.author))
         i = random.randint(0, len(loudMessages))
         if message.author.nick:
@@ -79,6 +89,7 @@ async def on_message(message):
             msg = loudMessages[i-1].format(message.author.name)
         await message.channel.send(msg)
 
+    logger.info("done processing")
     return
 
 @client.event
@@ -93,11 +104,7 @@ async def on_disconnect():
 async def on_resumed():
     logger.warning("dad-bot resuming")
 
-while True:
-    try:
-        logger.info("dad-bot starting")
-        client.loop.run_until_complete(client.start(TOKEN))
-        logger.info("dad-bot stopped running")
-    except BaseException:
-        time.sleep(5)
-
+logger.info("dad-bot starting")
+start_time = datetime.now()
+client.run(TOKEN)
+logger.info("dad-bot stopped running")
