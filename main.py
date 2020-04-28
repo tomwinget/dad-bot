bef3a386f2a6b62fb373acc029242de4c3883a30
Tom <tomwinget@gmail.com>
2020-04-27 18:02:04 -0700

    Added dad-on counter

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ import requests
 import logging
 import discord
 import traceback
+import redis
+import math
 
 
 TOKEN = os.environ['dadToken']
 vape_id = 436581339119222785
 client = discord.Client(max_message=None, heartbeat_timeout=30, assume_unsync_clock=True)
+r_local = redis.StrictRedis(decode_responses=True)
 
 
 dadPattern = r"^(?:i’m|i'm|im|i am) ?a? ([^\.\,\n]*)"
@@ -25,6 +28,8 @@ loudMessages = [
     'You\'re being too loud {}!'
 ]
 common_encoding = 'utf-8'
+USERS_HASH = "USERS"
+ordinal = lambda n: "%d%s" % (n,"tsnrhtdd"[(math.floor(n/10)%10!=1)*(n%10<4)*n%10::4])
 
 channel_topic = {
         'schoolhouse-rock': ['politic','trump','election','biden','president','dems','democrat','republican','cnn','fox']
@@ -77,13 +82,16 @@ async def on_message(message):
     logger.info("Checking for dadding")
     groups = re.search(dadPattern, str(message.content), re.IGNORECASE)
     if groups:
-        logger.info("dading on %s", str(message.author))
+        num_dads = ordinal(r_local.hincrby(USERS_HASH, message.author.id, 1))
+        logger.info("dading on %s for the %d time", str(message.author), num_dads)
         if len(message.mentions) >= 1:
             await message.author.edit(nick=f'Totally not {message.mentions[0].display_name[:20]}')
             await message.channel.send(f'Hi totally not {message.author.mention}, I\'m Dadbot!')
+            await message.channel.send(f'You\'ve been dadded for the {num_dads} time')
         else:
             await message.author.edit(nick=groups.group(1)[:32])
             await message.channel.send(f'Hi {message.author.mention}, I\'m Dadbot!')
+            await message.channel.send(f'You\'ve been dadded for the {num_dads} time')
 
     logger.info("Checking for yelling")
     if re.search(alphaPattern, str(message.content), re.IGNORECASE) \
@@ -99,10 +107,8 @@ async def on_message(message):
             if keyword in str(message.content).lower():
                 chan = [chann for chann in message.guild.channels if chann.name.lower() == channel]
                 logger.info("Found keyword: "+keyword+" and channels: "+str(chan))
-                if chan:
+                if chan and (channel not in message.channel.name):
                     await message.channel.send('Hey {}, I think that belongs in {}'.format(message.author.mention, chan[0].mention))
-                else:
-                    await message.channel.send('Hey {}, I think that belongs in {}'.format(message.author.mention, "#"+channel))
                 break
 
     logger.info("done processing")
